a580260b5ca88ceb266160443a3263bead2636e4
Cunev <cunev2005@gmail.com>
2025-10-28 14:00:28 +0300

    Fix

--- bashnya-hw3/main.go
+++ bashnya-hw3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"bashnya-hw3/deque"
 	"bashnya-hw3/stack"
 )
 
@@ -26,6 +27,24 @@ func runStackDemo() {
 	if err != nil {
 		fmt.Println("Ожидаемая ошибка:", err) // stack is empty
 	}
+}
+
+func runDequeDemo() {
+	fmt.Println("--- 2. Демонстрация: Deque ---")
+	deque := deque.NewDeque()
+	deque.PushBack(10) // [10]
+	deque.PushFront(5) // [5, 10]
+	deque.PushBack(20) // [5, 10, 20]
+
+	fmt.Println("Размер:", deque.Size()) // 3
+
+	item, _ := deque.PopFront()
+	fmt.Println("Извлекли спереди:", item) // 5
+
+	item, _ = deque.PopBack()
+	fmt.Println("Извлекли сзади:", item) // 20
+
+	fmt.Println("Новый размер:", deque.Size()) // 1
 }
 
 func main() {
